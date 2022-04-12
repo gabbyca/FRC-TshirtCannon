f418a4f192a6ea65c509715f35f7cf54d8800c41
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2022-04-11 20:28:51 -0400

    fixing some stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -148,7 +148,6 @@ public class Robot extends TimedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
-    m_robotContainer.configureTestBindings();
   }
 
   /**
